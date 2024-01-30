e18c577f51ca81b220d97d6ed3b610c0c4fd8778
sanovy <sanovy@git.com>
2024-01-30 09:18:59 +0100

    fix: BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { AppContainer } from "./components/AppContainer";
-import { Navbar } from "./components/Navbar";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
@@ -9,14 +8,12 @@ function App() {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <AppContainer>
-                <Navbar />
                 <Routes>
                     <Route index={true} path="/" element={<HomePage />}></Route>
                     {/* <Route path="/profile" element={}></Route>
                     <Route path="/skills" element={}></Route>
                     <Route path="/tasks" element={}></Route> */}
                 </Routes>
-                <HomePage />
             </AppContainer>
         </ThemeProvider>
     );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "@/components/Navbar";
 import { BaseContainer } from "../components/BaseContainer";
 import { CardWithForm } from "@/components/CardWithForm";
 
 const HomePage = () => {
     return (
         <BaseContainer>
+            <Navbar />
             <CardWithForm />
         </BaseContainer>
     );
