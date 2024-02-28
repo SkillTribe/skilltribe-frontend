1f048bd5b9ee9d03437ad1a64b07b9eb3d46b066
sanovy <sanovy@git.com>
2024-02-28 09:43:47 +0100

    roboto imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { AppContainer } from "./components/AppContainer";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
