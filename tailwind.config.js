57cc466861630bbff2e55c7b3ec7bb33b3219452
sanovy <sanovy@git.com>
2024-02-28 09:42:30 +0100

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+function myPlugin({ addUtilities }) {
+    const newUtilities = {
+        ".test": {
+            backgroundColor: "red",
+            textColors: "black",
+        },
+        ".center": {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+    };
+    addUtilities(newUtilities, ["responsive", "hover"]);
+}
+
 module.exports = {
+    plugins: [require("tailwindcss-animate"), myPlugin],
     darkMode: ["class"],
     content: [
         "./pages/**/*.{ts,tsx}",
@@ -9,6 +27,16 @@ module.exports = {
     ],
     prefix: "",
     theme: {
+        fontFamily: {
+            sans: ["Roboto"],
+            roboto: ["Roboto", "sans-serif"],
+        },
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
         container: {
             center: true,
             padding: "2rem",
@@ -17,6 +45,41 @@ module.exports = {
             },
         },
         extend: {
+            colors: {
+                primary: "rgba(244, 237, 219, 1)",
+                "primary-0.5": "rgba(244, 237, 219, 0.5)",
+                "primary-0.3": "rgba(244, 237, 219, 0.3)",
+                "primary-0.1": "rgba(244, 237, 219, 0.1)",
+                secondary: "rgba(75, 87, 97, 1)",
+                "secondary-0.8": "rgba(75, 87, 97, 0.8)",
+                "secondary-0.5": "rgba(75, 87, 97, 0.5)",
+                "secondary-0.3": "rgba(75, 87, 97, 0.3)",
+                "secondary-0.1": "rgba(75, 87, 97, 0.1)",
+                "black-0.5": "rgba(0, 0, 0, 0.50)",
+                "black-0.3": "rgba(0, 0, 0, 0.30)",
+                "black-0.1": "rgba(0, 0, 0, 0.10)",
+            },
+            backgroundColor: {
+                primary: "rgba(244, 237, 219, 1)",
+                "primary-0.5": "rgba(244, 237, 219, 0.5)",
+                "primary-0.3": "rgba(244, 237, 219, 0.3)",
+                "primary-0.1": "rgba(244, 237, 219, 0.1)",
+                secondary: "rgba(75, 87, 97, 1)",
+                "secondary-0.8": "rgba(75, 87, 97, 0.8)",
+                "secondary-0.5": "rgba(75, 87, 97, 0.5)",
+                "secondary-0.3": "rgba(75, 87, 97, 0.3)",
+                "secondary-0.1": "rgba(75, 87, 97, 0.1)",
+                "black-0.5": "rgba(0, 0, 0, 0.50)",
+                "black-0.3": "rgba(0, 0, 0, 0.30)",
+                "black-0.1": "rgba(0, 0, 0, 0.10)",
+            },
+            spacing: {
+                128: "32rem",
+                144: "36rem",
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
             keyframes: {
                 "accordion-down": {
                     from: { height: "0" },
@@ -33,5 +96,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    important: true,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
+    root: "./",
+
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "src/assets/media",
+                    dest: "src/assets",
+                },
+                {
+                    src: "src/assets/site",
+                    dest: "src/assets",
+                },
+            ],
+        }),
+    ],
     build: {
         outDir: "build",
         target: "es2020",
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split(".").at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = "img";
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: "assets/js/[name]-[hash].js",
+                entryFileNames: "assets/js/[name]-[hash].js",
+            },
+        },
     },
+    // publicDir: "./src/assets",
     server: {
         open: true,
         port: 3000,
